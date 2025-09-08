31baa77ec000fca523aa4c0273ad8efa37bc94d0
Valeriy Vershkov <thedoorinto@yandex.ru>
2025-09-08 13:58:11 +0400

    add new schema MovieCreate

--- movies-api/schemas/movies.py
+++ movies-api/schemas/movies.py
@@ -1,8 +1,20 @@
+from typing import Annotated
 from pydantic import BaseModel
 
 
 class Movie(BaseModel):
     id: int
+    title: str
+    description: str
+    genre: str
+    year: int
+
+
+class MovieCreate(BaseModel):
+    """
+    Модель для создания фильма.
+    """
+
     title: str
     description: str
     genre: str
