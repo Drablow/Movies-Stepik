1679d97e551d92ab6c8c437bbb1aa7bba6a8558a
Valeriy Vershkov <thedoorinto@yandex.ru>
2025-09-08 13:29:09 +0400

    установка multipart и новый роут

--- movies-api/api/api_v1/movies/vievs.py
+++ movies-api/api/api_v1/movies/vievs.py
@@ -1,5 +1,8 @@
 from typing import Annotated
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter, status, Form
+
+from random import randint
+
 
 from schemas.movies import Movie
 
@@ -15,6 +18,26 @@ router = APIRouter(
 @router.get("/", response_model=list[Movie])
 def get_all_movies():
     return MOVIES
+
+
+@router.post(
+    "/",
+    response_model=Movie,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_movie(
+    title: Annotated[str, Form()],
+    description: Annotated[str, Form()],
+    genre: Annotated[str, Form()],
+    year: Annotated[int, Form()],
+):
+    return Movie(
+        id=randint(4, 10),
+        title=title,
+        description=description,
+        genre=genre,
+        year=year,
+    )
 
 
 @router.get("/{movie_id}", response_model=Movie)
